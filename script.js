3d79533435d14258c143e5915448a3eeabb8de12
Rohitsah6372 <91113994+Rohitsah6372@users.noreply.github.com>
2024-08-29 20:32:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 function stringChop(str, size) {
-  // your code here
 	if(!str)
 		return [];
-
 	let ans = [];
-
 	for(let i=0;i<str.length;i=i+size){
 		let sliced = str.slice(i, i+size);
 		ans.push(sliced);
 	}
-
 	return ans;
-	
 }
+
+console.log(stringChop("Hello, world!", 5));
 
 Do not change the code below
 const str = prompt("Enter String.");
